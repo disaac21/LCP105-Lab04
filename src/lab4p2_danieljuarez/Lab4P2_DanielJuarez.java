b1aa5978b68cd48d7ab84b16ad424fff05a61f68
Your Name <danielisaacjuarez@hmail.com>
2022-05-13 15:29:37 -0600

    Solo Falta Jugar

--- src/lab4p2_danieljuarez/Lab4P2_DanielJuarez.java
+++ src/lab4p2_danieljuarez/Lab4P2_DanielJuarez.java
@@ -2,6 +2,7 @@ package lab4p2_danieljuarez;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Lab4P2_DanielJuarez {
@@ -18,14 +19,14 @@ public class Lab4P2_DanielJuarez {
         spidermans.add(new SuperiorSpiderman("Superior 2", 100, 500, "Universo 2"));
         spidermans.add(new Clasico("Clásico 1", 100, 500, "Universo 3"));
         spidermans.add(new Clasico("Clásico 2", 100, 500, "Universo 3"));
-        
+
         malos.add(new Blindado("Blindado 1", 100, 500, "Universo 1"));
         malos.add(new Blindado("Blindado 2", 100, 500, "Universo 1"));
         malos.add(new Alienígena("Alienigena 1", 100, 500, "Universo 2"));
         malos.add(new Alienígena("Alienigena 2", 100, 500, "Universo 2"));
         malos.add(new Alterado("Alterado 1", 100, 500, "Universo 3"));
         malos.add(new Alterado("Alterado 2", 100, 500, "Universo 3"));
-        
+
         int menu = 1;
         while (menu != 0) {
             System.out.println("0. Salir\n"
@@ -50,11 +51,54 @@ public class Lab4P2_DanielJuarez {
                 }
                 break;
                 case 3: {
+                    for (int i = 0; i < spidermans.size(); i++) {
+                        System.out.println(i + "-" + spidermans.get(i));
+                    }
+
+                    System.out.print("Ingrese el Spiderman a Modificar: ");
+                    int modspider = lea.nextInt();
+                    lea.nextLine();
+                    System.out.println("Ingrese el Nombre: ");
+                    String nombre = lea.nextLine();
+                    System.out.println("Ingrese los Puntos de Ataque: ");
+                    int puntosataque = lea.nextInt();
+                    System.out.println("Ingrese los Puntos de Vida: ");
+                    int puntosvida = lea.nextInt();
+                    lea.nextLine();
+                    System.out.println("Ingrese el Universo: ");
+                    String universo = lea.nextLine();
+
+                    spidermans.get(modspider).setNombre(nombre);
+                    spidermans.get(modspider).setPuntosataque(puntosataque);
+                    spidermans.get(modspider).setPuntosvida(puntosvida);
+                    spidermans.get(modspider).setNombreuniverso(universo);
 
                 }
                 break;
                 case 4: {
+                    
+                    for (int i = 0; i < malos.size(); i++) {
+                        System.out.println(i + "-" + malos.get(i));
+                    }
 
+                    System.out.print("Ingrese el Villano a Modificar: ");
+                    int modmalos = lea.nextInt();
+                    lea.nextLine();
+                    System.out.println("Ingrese el Nombre: ");
+                    String nombre = lea.nextLine();
+                    System.out.println("Ingrese los Puntos de Ataque: ");
+                    int puntosataque = lea.nextInt();
+                    System.out.println("Ingrese los Puntos de Vida: ");
+                    int puntosvida = lea.nextInt();
+                    lea.nextLine();
+                    System.out.println("Ingrese el Universo: ");
+                    String universo = lea.nextLine();
+
+                    malos.get(modmalos).setNombre(nombre);
+                    malos.get(modmalos).setPuntosataque(puntosataque);
+                    malos.get(modmalos).setPuntosvida(puntosvida);
+                    malos.get(modmalos).setNombreuniverso(universo);
+                    
                 }
                 break;
                 case 5: {
@@ -65,4 +109,10 @@ public class Lab4P2_DanielJuarez {
             } // fin switch menu
         } //fin while menu
     } // fin main
+
+    public static Personaje Modificar() {
+        //nombre, puntos de ataque, puntos de vida, nombre de universo.
+        return null;
+    }
+
 } // fin clase
