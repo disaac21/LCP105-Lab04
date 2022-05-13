a4cbff4498f9c47b7a8eafc4eff453871e684824
Your Name <danielisaacjuarez@hmail.com>
2022-05-13 14:17:50 -0600

    Clases Spidermans

--- src/lab4p2_danieljuarez/Lab4P2_DanielJuarez.java
+++ src/lab4p2_danieljuarez/Lab4P2_DanielJuarez.java
@@ -1,21 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab4p2_danieljuarez;
 
-/**
- *
- * @author danie
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab4P2_DanielJuarez {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner lea = new Scanner(System.in);
+    static ArrayList<Personaje> spidermans = new ArrayList();
+    static ArrayList<Personaje> malos = new ArrayList();
+
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+
+        int menu = 1;
+        while (menu != 0) {
+            System.out.println("0. Salir\n"
+                    + "1. Listar Spiderman\n"
+                    + "2. Listar Enemigos\n"
+                    + "3. Modificar Spiderman\n"
+                    + "4. Modificar Enemigos\n"
+                    + "5. Jugar");
+            menu = lea.nextInt();
+            System.out.println();
+
+            switch (menu) {
+                case 1: {
+
+                }
+                break;
+                case 2: {
+
+                }
+                break;
+                case 3: {
+
+                }
+                break;
+                case 4: {
+
+                }
+                break;
+                case 5: {
+
+                }
+                break;
+            } // fin switch menu
+        } //fin while menu
+    } // fin main
+} // fin clase
---
+++ src/lab4p2_danieljuarez/SpiderPunk.java
@@ -0,0 +1,55 @@
+package lab4p2_danieljuarez;
+
+public class SpiderPunk extends Personaje {
+
+    public SpiderPunk() {
+        super();
+    }
+    
+    public SpiderPunk(String string, double d, double d1, String string1) {
+        super(string, d, d1, string1);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPuntosataque() {
+        return puntosataque;
+    }
+
+    public void setPuntosataque(double puntosataque) {
+        this.puntosataque = puntosataque;
+    }
+
+    public double getPuntosvida() {
+        return puntosvida;
+    }
+
+    public void setPuntosvida(double puntosvida) {
+        this.puntosvida = puntosvida;
+    }
+
+    public String getNombreuniverso() {
+        return nombreuniverso;
+    }
+
+    public void setNombreuniverso(String nombreuniverso) {
+        this.nombreuniverso = nombreuniverso;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    public double Ataque(){
+        
+        return 0;
+    }
+    
+}
